5f5679717b39ece1dd6d15ea75224b5fb58d6db5
HELLISH666-warframe <102866355+HELLISH666-warframe@users.noreply.github.com>
2026-04-07 00:02:18 +1000

    Shmit.

--- data/global.hx
+++ data/global.hx
@@ -25,65 +25,64 @@ static var redirectStates:Map<FlxState, String> = [
 
 function new() {
     Handle.init([]);
+    FlxG.save.bind('V2', 'Gorefield');
 
     // MECHANICS
-    if (FlxG.save.data.baby == null) FlxG.save.data.baby = false;
-    if (FlxG.save.data.ps_hard == null) FlxG.save.data.ps_hard = false;
-    if (FlxG.save.data.scare_hard == null) FlxG.save.data.scare_hard = false;
-    if (FlxG.save.data.blue_hard == null) FlxG.save.data.blue_hard = false;
-    if (FlxG.save.data.orange_hard == null) FlxG.save.data.orange_hard = false;
+    FlxG.save.data.baby ??= false;
+    FlxG.save.data.ps_hard ??= false;
+    FlxG.save.data.scare_hard ??= false;
+    FlxG.save.data.blue_hard ??= false;
+    FlxG.save.data.orange_hard ??= false;
 
     // VISUALS
-    if (FlxG.save.data.bloom == null) FlxG.save.data.bloom = true;
-    if (FlxG.save.data.glitch == null) FlxG.save.data.glitch = true;
-    if (FlxG.save.data.warp == null) FlxG.save.data.warp = true;
-    if (FlxG.save.data.wrath == null) FlxG.save.data.wrath = true;
-    if (FlxG.save.data.heatwave == null) FlxG.save.data.heatwave = true;
-    if (FlxG.save.data.static == null) FlxG.save.data.static = true;
-    if (FlxG.save.data.drunk == null) FlxG.save.data.drunk = true;
-    if (FlxG.save.data.vhs == null) FlxG.save.data.vhs = true;
+    FlxG.save.data.bloom ??= true;
+    FlxG.save.data.glitch ??= true;
+    FlxG.save.data.warp ??= true;
+    FlxG.save.data.wrath ??= true;
+    FlxG.save.data.heatwave ??= true;
+    FlxG.save.data.static ??= true;
+    FlxG.save.data.drunk ??= true;
+    FlxG.save.data.vhs ??= true;
+    FlxG.save.data.saturation ??= true;
 
-    if (FlxG.save.data.drunk == null) FlxG.save.data.drunk = true;
-    if (FlxG.save.data.saturation == null) FlxG.save.data.saturation = true;
-
-    if (FlxG.save.data.trails == null) FlxG.save.data.trails = true;
-    if (FlxG.save.data.particles == null) FlxG.save.data.particles = true;
-    if (FlxG.save.data.flashing == null) FlxG.save.data.flashing = true;
+    FlxG.save.data.trails ??= true;
+    FlxG.save.data.particles ??= true;
+    FlxG.save.data.flashing ??= true;
 
     //PROGRESSION
-    if (FlxG.save.data.weeksFinished == null) FlxG.save.data.weeksFinished = [false, false, false, false, false, false];
-    if (FlxG.save.data.codesUnlocked == null) FlxG.save.data.codesUnlocked = false;
-    if (FlxG.save.data.weeksUnlocked == null) FlxG.save.data.weeksUnlocked = [true, false, false, false, false, false, false, false];
+    FlxG.save.data.weeksFinished ??= [false, false, false, false, false, false];
+    FlxG.save.data.codesUnlocked ??= false;
+    FlxG.save.data.weeksUnlocked ??= [true, false, false, false, false, false, false, false];
 
-    if (FlxG.save.data.beatWeekG1 == null) FlxG.save.data.beatWeekG1 = false;
-    if (FlxG.save.data.beatWeekG2 == null) FlxG.save.data.beatWeekG2 = false;
-    if (FlxG.save.data.beatWeekG3 == null) FlxG.save.data.beatWeekG3 = false;
-    if (FlxG.save.data.beatWeekG4 == null) FlxG.save.data.beatWeekG4 = false;
-    if (FlxG.save.data.beatWeekG5 == null) FlxG.save.data.beatWeekG5 = false;
-    if (FlxG.save.data.beatWeekG6 == null) FlxG.save.data.beatWeekG6 = false;
-    if (FlxG.save.data.beatWeekG7 == null) FlxG.save.data.beatWeekG7 = false;
-    if (FlxG.save.data.beatWeekG8 == null) FlxG.save.data.beatWeekG8 = false;
-    if (FlxG.save.data.firstTimeLanguage == null) FlxG.save.data.firstTimeLanguage = true;
+    FlxG.save.data.beatWeekG1 ??= false;
+    FlxG.save.data.beatWeekG2 ??= false;
+    FlxG.save.data.beatWeekG3 ??= false;
+    FlxG.save.data.beatWeekG4 ??= false;
+    FlxG.save.data.beatWeekG5 ??= false;
+    FlxG.save.data.beatWeekG6 ??= false;
+    FlxG.save.data.beatWeekG7 ??= false;
+    FlxG.save.data.beatWeekG8 ??= false;
+    FlxG.save.data.firstTimeLanguage ??= true;
 
-    if(FlxG.save.data.weekProgress == null) FlxG.save.data.weekProgress = ["" => {}];
+    FlxG.save.data.weekProgress ??= ["" => {}];
     weekProgress = FlxG.save.data.weekProgress;
     // CODES 
-    if (FlxG.save.data.extrasSongs == null) FlxG.save.data.extrasSongs = [];
-    if (FlxG.save.data.extrasSongsIcons == null) FlxG.save.data.extrasSongsIcons = [];
-    if (FlxG.save.data.codesList == null) FlxG.save.data.codesList = ["HUMUNGOSAURIO", "PUEBLO MARRON", "ALTERCAT"];
+    FlxG.save.data.extrasSongs ??= [];
+    FlxG.save.data.extrasSongsIcons ??= [];
+    FlxG.save.data.codesList ??= ["HUMUNGOSAURIO", "PUEBLO MARRON", "ALTERCAT"];
 
     // EASTER EGG
-    if (FlxG.save.data.canVisitArlene == null) FlxG.save.data.canVisitArlene = false;
-    if (FlxG.save.data.hasVisitedPhase == null) FlxG.save.data.hasVisitedPhase = false;
-    if (FlxG.save.data.paintPosition == null) FlxG.save.data.paintPosition = -1;
-    if (FlxG.save.data.arlenePhase == null) FlxG.save.data.arlenePhase = 0;
+    FlxG.save.data.canVisitArlene ??= false;
+    FlxG.save.data.hasVisitedPhase ??= false;
+    FlxG.save.data.paintPosition ??= -1;
+    FlxG.save.data.arlenePhase ??= 0;
     
     // CREDITS
-    if (FlxG.save.data.alreadySeenCredits == null) FlxG.save.data.alreadySeenCredits = false;
+    FlxG.save.data.alreadySeenCredits ??= false;
 
     // OTHER
-    if (FlxG.save.data.spanish == null) FlxG.save.data.spanish = false;
-    if (FlxG.save.data.dev == null) FlxG.save.data.dev = false;
+    FlxG.save.data.spanish ??= false;
+    FlxG.save.data.dev ??= false;
 
     Lib.application.onExit.add(function(i:Int) {
         FlxG.save.data.weekProgress = weekProgress;
@@ -106,4 +105,6 @@ function preStateSwitch() {
             FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
 }
 
-function destroy() FlxG.camera.bgColor = 0xFF000000;
+function destroy() {FlxG.camera.bgColor = 0xFF000000;
+    FlxG.save.bind('save-default', 'CodenameEngine');
+}
